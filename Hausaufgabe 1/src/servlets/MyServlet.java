8a080450a52d98beb3c9778482bffd390b8202bb
Ronny Jauch <ronny.jauch@outlook.com>
2019-11-11 09:38:26 +0100

    removed comment from MyServlet

--- Hausaufgabe 1/src/servlets/MyServlet.java
+++ Hausaufgabe 1/src/servlets/MyServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class MyServlet Kommentar
+ * Servlet implementation class MyServlet
  */
 @WebServlet("/MyServlet")
 public class MyServlet extends HttpServlet {
