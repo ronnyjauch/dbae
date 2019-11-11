0a27489a9dc67f767f6ffa119b893b4de8e2f233
Ronny Jauch <ronny.jauch@outlook.com>
2019-11-11 09:35:46 +0100

    changes comment in MyServlet

--- Hausaufgabe 1/src/servlets/MyServlet.java
+++ Hausaufgabe 1/src/servlets/MyServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class MyServlet
+ * Servlet implementation class MyServlet Kommentar
  */
 @WebServlet("/MyServlet")
 public class MyServlet extends HttpServlet {
